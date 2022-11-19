e530e98854c4744b6bd14dc6d83d9276ef15ab46
Rahmadika S Setiawan <dikakewren@outlook.com>
2022-11-19 11:32:13 +0700

    MVC TOMBOL KJP

--- application/views/SPP/tampilan_detail_pembayaran_spp.php
+++ application/views/SPP/tampilan_detail_pembayaran_spp.php
@@ -1,7 +1,6 @@
 <div class="alert alert-success" role="alert">
     <h5 class="text-center text-uppercase font-weight-bold">detail pembayaran spp</h5>
 </div>
-
 <a class="btn btn-sm btn-success text-uppercase font-weight-bold mb-2" href="<?= base_url() ?>Dashboard/pembayaran_spp_per_nis/<?= $siswa['nis'] ?>">kembali</a>
 <div class="row">
     <div class="col-md">
@@ -100,14 +99,21 @@
                                                 <input type="text" class="form-control text-uppercase" name="bulan" value="<?= $row['bulan'] ?>" hidden>
                                                 <input type="text" class="form-control text-uppercase" name="cash" value="<?= $row['nominal'] ?>" hidden>
                                                 <h5 class="text-center ">
-                                                    <button type="submit" class="badge badge-primary text-uppercase">non kjp</button>
+                                                    <button type="submit" class="btn btn-primary btn-sm btn-block text-uppercase font-weight-bold">non kjp</button>
                                                 </h5>
                                             </form>
                                         </div>
                                         <div class="col-md">
-                                            <h5 class="text-center text-uppercase">
-                                                <a class="badge badge-danger" href="<?= base_url() ?>Dashboard/bayar_spp_kjp/<?= $row['id_spp_siswa']; ?>">KJP</a>
-                                            </h5>
+                                            <form action="<?= base_url() ?>Dashboard/simpan_spp_kjp/<?= $row['id_spp_siswa'] ?>" method="POST">
+                                                <input type="text" class="form-control text-uppercase" name="id_spp_siswa" value="<?= $row['id_spp_siswa'] ?>" hidden>
+                                                <input type="text" class="form-control text-uppercase" name="id_siswa" value="<?= $row['id_siswa'] ?>" hidden>
+                                                <input type="text" class="form-control text-uppercase" name="kode_bulan" value="<?= $row['kode_bulan'] ?>" hidden>
+                                                <input type="text" class="form-control text-uppercase" name="bulan" value="<?= $row['bulan'] ?>" hidden>
+                                                <input type="text" class="form-control text-uppercase" name="kjp_cash" value="<?= $row['nominal'] - '240000' ?>" hidden>
+                                                <h5 class="text-center ">
+                                                    <button type="submit" class="btn btn-danger btn-sm btn-block text-uppercase font-weight-bold">kjp</button>
+                                                </h5>
+                                            </form>
                                         </div>
                                     </div>
                                 </td>
