50ee6da20f9590addfc5646dc4e6e248b8dfc7c5
Rahmadika S Setiawan <dikakewren@outlook.com>
2023-02-01 12:01:20 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,11 @@ date_default_timezone_set("Asia/jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sppth_terbaru';
+// $config['base_url'] = 'http://localhost/sppth_terbaru';
+
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
