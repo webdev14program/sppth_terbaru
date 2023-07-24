8fddb21c23038d6f84a803241e3ea5d4dc81e2a4
Rahmadika S Setiawan <dikakewren@outlook.com>
2023-07-24 09:24:14 +0700

    Update tampilan_setting_sistem.php

--- application/views/tampilan_setting_sistem.php
+++ application/views/tampilan_setting_sistem.php
@@ -4,9 +4,62 @@
 
 <div class="row">
 	<div class="col-md">
-		<div class="jumbotron jumbotron-fluid alert alert-dark">
-			<h3 class="display-4 text-center font-weight-bold mt-5">MAAF FITUR INI SEDANG DALAM PERBAIKAN</h3>
-			<h5 class="text-center text-uppercase mt-5"><a class="btn btn-primary btn-lg font-weight-bold" href="<?= base_url() ?>Dashboard" role="button">kembali ke Dashboard</a></h5>
+		<div class="card">
+			<div class="card-header bg-gradient-danger text-white">
+				<h5 class="text-center text-uppercase font-weight-bold">Hapus</h5>
+			</div>
+			<div class="card-body">
+				<div class="row">
+					<div class="col-md">
+						<div class="card">
+							<div class="card-header bg-gradient-success text-white text-center">
+								<h5 class="text-uppercase font-weight-bold">spp</h5>
+							</div>
+							<div class="card-body">
+								<h5 class="text-center text-uppercase font-weight-bold">
+									<a class="btn btn-danger btn-sm" href="">Hapus</a>
+								</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-md">
+						<div class="card">
+							<div class="card-header bg-gradient-info text-white text-center">
+								<h5 class="text-uppercase font-weight-bold">adm lain</h5>
+							</div>
+							<div class="card-body">
+								<h5 class="text-center text-uppercase font-weight-bold">
+									<a class="btn btn-danger btn-sm" href="">Hapus</a>
+								</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-md">
+						<div class="card">
+							<div class="card-header bg-gradient-primary text-white text-center">
+								<h5 class="text-uppercase font-weight-bold">uang masuk</h5>
+							</div>
+							<div class="card-body">
+								<h5 class="text-center text-uppercase font-weight-bold">
+									<a class="btn btn-danger btn-sm" href="">Hapus</a>
+								</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-md">
+						<div class="card">
+							<div class="card-header bg-gradient-dark text-white text-center">
+								<h5 class="text-uppercase font-weight-bold">daftar ulang</h5>
+							</div>
+							<div class="card-body">
+								<h5 class="text-center text-uppercase font-weight-bold">
+									<a class="btn btn-danger btn-sm" href="">Hapus</a>
+								</h5>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
