95411af2ca75c1cd620493069dcbab426c03dc7a
Rahmadika S Setiawan <dikakewren@outlook.com>
2023-07-30 21:39:37 +0700

    MVC MAINTENANCe FITUR NIS

--- application/views/Siswa/tampilan_nis_siswa_tahun_ajaran.php
+++ application/views/Siswa/tampilan_nis_siswa_tahun_ajaran.php
@@ -1,38 +1,13 @@
- <div class="alert alert-success" role="alert">
+ <div class="alert alert-danger" role="alert">
      <h5 class="text-center text-uppercase font-weight-bold">NOMOR INDUK SISWA</h5>
  </div>
- <div class="card mt-2">
-     <div class="card-body">
-         <div class="table-responsive">
-             <table class="table table-striped table-bordered" id="dataTable" width="100%" cellspacing="0">
-                 <thead>
-                     <tr class="text-center text-uppercase font-weight-bold">
-                         <th>#</th>
-                         <th>ID tahun ajaran kelas</th>
-                         <th>tahun ajaran</th>
-                         <th>jumlah kelas</th>
-                         <th>Aksi</th>
-                     </tr>
-                 </thead>
-                 <tbody>
-                     <tr>
-                         <?php
-                            $no = 1;
-                            foreach ($tahun_ajaran as $row) {
-                            ?>
-                             <td class="text-center text-uppercase font-weight-bold"><?php echo $no++; ?></td>
-                             <td class="text-center text-uppercase font-weight-bold"><?= $row['id_tahun_ajaran']; ?></td>
-                             <td class="text-center text-uppercase font-weight-bold"><?= $row['tahun_ajaran']; ?></td>
-                             <td class="text-center text-uppercase font-weight-bold"><?= $row['jumlah_kelas']; ?> Kelas</td>
-                             <td>
-                                 <h5 class="text-center">
-                                     <a class="btn btn-primary btn-sm text-uppercase font-weight-bold" href="<?= base_url() ?>Peserta_didik/detail_nis_kelas_tahun_ajaran/<?= $row['id_tahun_ajaran']; ?>">Detail</a>
-                                 </h5>
-                             </td>
-                     </tr>
-                 <?php } ?>
-                 </tbody>
-             </table>
+ <div class="row">
+     <div class="col-md">
+         <div class="jumbotron alert alert-danger text-center">
+             <h1 class="display-4">MAINTENANCE</h1>
+             <h1 class="display-4">FITUR INI MASIH DALAM PERBAIKAN</h1>
+             <hr class="my-4">
+             <a class="btn btn-primary btn-lg" href="<?= base_url() ?>Dashboard" role="button">DASHBOARD</a>
          </div>
      </div>
  </div>
