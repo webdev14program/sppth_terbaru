ec3190502ef08d375e4a06181cbdef762208ee95
Rahmadika S Setiawan <dikakewren@outlook.com>
2023-09-23 21:00:26 +0700

    MVC UPDATE

--- application/controllers/Tahun_ajaran.php
+++ application/controllers/Tahun_ajaran.php
@@ -1,9 +1,10 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Tahun_ajaran extends CI_Controller {
+class Tahun_ajaran extends CI_Controller
+{
 
-	
+
 	public function index()
 	{
 		$isi['tahun_ajaran'] = $this->Model_tahun_ajaran->dataTahunAjaran();
@@ -13,5 +14,31 @@ class Tahun_ajaran extends CI_Controller {
 		$this->load->view('templates/header');
 		$this->load->view('tampilan_dashboard', $isi);
 		$this->load->view('templates/footer');
+	}
+
+	public  function simpan()
+	{
+
+		$id_tahun_ajaran = rand(111111, 999999);
+		$tahun_ajaran = $this->input->post('tahun_ajaran');
+		$status = 'AKTIF';
+
+		$data = array(
+			'id_tahun_ajaran' => $id_tahun_ajaran,
+			'tahun_ajaran' => $tahun_ajaran,
+			'status' => $status,
+		);
+		$this->db->insert('tahun_ajaran', $data);
+		$this->session->set_flashdata('info', '<div class="row">
+        <div class="col-md mt-2">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>TAHUN BERHASIL DI SIMPAN</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+        </div>');
+		redirect('Tahun_ajaran');
 	}
 }
--- application/views/tampilan_data_tahun_ajaran.php
+++ application/views/tampilan_data_tahun_ajaran.php
@@ -2,32 +2,73 @@
     <h5 class="text-center text-uppercase font-weight-bold">tahun ajaran</h5>
 </div>
 
-<div class="card mt-2">
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-striped table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                    <tr class="text-center text-uppercase font-weight-bold">
-                        <th>#</th>
-                        <th>ID</th>
-                        <th>tahun ajaran</th>
-                        <th>status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <?php
-                        $no = 1;
-                        foreach ($tahun_ajaran as $row) {
-                        ?>
-                            <td class="text-center text-uppercase font-weight-bold"><?php echo $no++; ?></td>
-                            <td class="text-center text-uppercase font-weight-bold"><?= $row['id_tahun_ajaran']; ?></td>
-                            <td class="text-center text-uppercase font-weight-bold">Tahun Ajaran <?= $row['tahun_ajaran']; ?></td>
-                            <td class="text-center text-uppercase font-weight-bold"><?= $row['status_tahun_ajaran']; ?></td>
-                    </tr>
-                <?php } ?>
-                </tbody>
-            </table>
+<div class="row">
+    <div class="col-md">
+        <div class="card">
+            <div class="card-body">
+                <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModal">
+                    Tambah Data
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?= $this->session->flashdata('info') ?>
+
+<div class="row">
+    <div class="col-md">
+        <div class="card mt-2">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr class="text-center text-uppercase font-weight-bold">
+                                <th>#</th>
+                                <th>ID</th>
+                                <th>tahun ajaran</th>
+                                <th>status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <?php
+                                $no = 1;
+                                foreach ($tahun_ajaran as $row) {
+                                ?>
+                                    <td class="text-center text-uppercase font-weight-bold"><?php echo $no++; ?></td>
+                                    <td class="text-center text-uppercase font-weight-bold"><?= $row['id_tahun_ajaran']; ?></td>
+                                    <td class="text-center text-uppercase font-weight-bold">Tahun Ajaran <?= $row['tahun_ajaran']; ?></td>
+                                    <td class="text-center text-uppercase font-weight-bold"><?= $row['status_tahun_ajaran']; ?></td>
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-primary text-white text-uppercase">
+                <h5 class="modal-title">Tambah Tahun Ajaran</h5>
+            </div>
+            <div class="modal-body">
+                <form method="post" action="<?= base_url() ?>Tahun_ajaran/simpan">
+                    <div class="form-group">
+                        <label>Tahun Ajaran</label>
+                        <input type="text" class="form-control" name="tahun_ajaran" aria-describedby="emailHelp">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+            </div>
+
         </div>
     </div>
 </div>
