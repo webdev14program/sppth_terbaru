de029a13aa6c2234ce66c074d8c915b4ae2aa9a8
Rahmadika S Setiawan <dikakewren@outlook.com>
2024-09-21 19:21:12 +0700

    UPDATE

--- application/config/database.php
+++ application/config/database.php
@@ -27,24 +27,4 @@ $db['default'] = array(
 	'save_queries' => TRUE
 );
 
-// $db['default'] = array(
-// 	'dsn'	=> '',
-// 	'hostname' => 'srv144.niagahoster.com',
-// 	'username' => 'webd1830_sppth',
-// 	'password' => 'Rdss1995',
-// 	'database' => 'webd1830_sppth',
-// 	'dbdriver' => 'mysqli',
-// 	'dbprefix' => '',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
+
